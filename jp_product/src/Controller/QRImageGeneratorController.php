5166cd282f5ab5f8d1e3939bd70a40d0ed6e07d2
Ganesh <ganeshjadhav008@gmail.com>
2022-05-16 20:44:44 +0530

    Accordinnovations-Task for Jugaad Patches Pvt Ltd create QR code for purches product

---
+++ jp_product/src/Controller/QRImageGeneratorController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\jp_product\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\jp_product\Response\QRImageResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Controller which generates the image from defined settings.
+ */
+class QRImageGeneratorController extends ControllerBase {
+
+  /**
+   * Request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $request;
+
+  /**
+   * QRImageGeneratorController constructor.
+   *
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request
+   *   Request object to get request params.
+   */
+  public function __construct(RequestStack $request) {
+    $this->request = $request;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * LogoWidth.
+   *
+   * @return int
+   *   Will return the logo width.
+   */
+  public function getLogoWidth() {
+    return $this->config('jp_product.settings')->get('logo_width');
+  }
+
+  /**
+   * LogoSize.
+   *
+   * @return int
+   *   Will return the logo size.
+   */
+  public function getLogoSize() {
+    return $this->config('jp_product.settings')->get('set_size');
+  }
+
+  /**
+   * LogoMargin.
+   *
+   * @return int
+   *   Will return the logo margin.
+   */
+  public function getLogoMargin() {
+    return $this->config('jp_product.settings')->get('set_margin');
+  }
+
+  /**
+   * Will return the response for external url.
+   *
+   * @return \Drupal\jp_product\Response\QRImageRespons
+   *   Will return the image response.
+   */
+  public function withUrl() {
+    $externalUrl = $this->request->getCurrentRequest()->query->get('path');
+    return new QRImageResponse($externalUrl, $this->getLogoWidth(), $this->getLogoSize(), $this->getLogoMargin());
+  }
+
+}
